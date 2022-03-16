25b5011636c1f5538de1b80cfe598edebdd1bd72
Cheranga Hatangala <cheranga@gmail.com>
2022-03-17 05:14:25 +1100

    removed commented code.

--- Demo.Kodez.Customers.Identity.Api/Extensions/ConfigurationBuilderExtensions.cs
+++ Demo.Kodez.Customers.Identity.Api/Extensions/ConfigurationBuilderExtensions.cs
@@ -31,32 +31,6 @@ namespace Demo.Kodez.Customers.Identity.Api.Extensions
                         .ConfigureRefresh(refreshOptions => { refreshOptions.Register("RefreshAll", true).SetCacheExpiration(TimeSpan.FromSeconds(5)); })
                         .UseFeatureFlags(flagOptions => { flagOptions.CacheExpirationInterval = TimeSpan.FromSeconds(5); });
                 });
-
-            // webHostBuilder.ConfigureAppConfiguration(builder =>
-            // {
-            //     var config = builder.Build();
-            //     var identityAzureAppConfigurationUri = config["IdentityAzureAppConfigurationUrl"];
-            //     var sharedAzureAppConfigurationUri = config["SharedAzureAppConfigurationUrl"];
-            //
-            //     var credentials = new DefaultAzureCredential();
-            //
-            //     builder.AddAzureAppConfiguration(options =>
-            //         {
-            //             options
-            //                 .Connect(new Uri(sharedAzureAppConfigurationUri), credentials)
-            //                 .ConfigureKeyVault(vaultOptions => { vaultOptions.SetCredential(credentials); })
-            //                 .ConfigureRefresh(refreshOptions => { refreshOptions.Register("RefreshAll", true).SetCacheExpiration(TimeSpan.FromSeconds(5)); })
-            //                 .UseFeatureFlags(flagOptions => { flagOptions.CacheExpirationInterval = TimeSpan.FromSeconds(5); });
-            //         })
-            //         .AddAzureAppConfiguration(options =>
-            //         {
-            //             options
-            //                 .Connect(new Uri(identityAzureAppConfigurationUri), credentials)
-            //                 .ConfigureKeyVault(vaultOptions => { vaultOptions.SetCredential(credentials); })
-            //                 .ConfigureRefresh(refreshOptions => { refreshOptions.Register("RefreshAll", true).SetCacheExpiration(TimeSpan.FromSeconds(5)); })
-            //                 .UseFeatureFlags(flagOptions => { flagOptions.CacheExpirationInterval = TimeSpan.FromSeconds(5); });
-            //         });
-            // });
         }
     }
 }
